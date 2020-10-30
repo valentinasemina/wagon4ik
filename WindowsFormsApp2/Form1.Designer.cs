cc3b3317ef4b379d02dcb7dbb2cba37f7b22314d
valentinasemina <valyaseminav@gmail.com>
2020-10-30 13:57:27 +0300

    Added project

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbRectangle = new System.Windows.Forms.RadioButton();
+            this.rbCircle = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.btcreate = new System.Windows.Forms.Button();
+            this.btpar = new System.Windows.Forms.Button();
+            this.rbwagon = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // rbRectangle
+            // 
+            this.rbRectangle.AutoSize = true;
+            this.rbRectangle.Location = new System.Drawing.Point(6, 9);
+            this.rbRectangle.Name = "rbRectangle";
+            this.rbRectangle.Size = new System.Drawing.Size(74, 17);
+            this.rbRectangle.TabIndex = 0;
+            this.rbRectangle.TabStop = true;
+            this.rbRectangle.Text = "Rectangle";
+            this.rbRectangle.UseVisualStyleBackColor = true;
+            this.rbRectangle.CheckedChanged += new System.EventHandler(this.rbRectangle_CheckedChanged);
+            // 
+            // rbCircle
+            // 
+            this.rbCircle.AutoSize = true;
+            this.rbCircle.Location = new System.Drawing.Point(6, 32);
+            this.rbCircle.Name = "rbCircle";
+            this.rbCircle.Size = new System.Drawing.Size(51, 17);
+            this.rbCircle.TabIndex = 1;
+            this.rbCircle.TabStop = true;
+            this.rbCircle.Text = "Circle";
+            this.rbCircle.UseVisualStyleBackColor = true;
+            this.rbCircle.CheckedChanged += new System.EventHandler(this.rbCircle_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Window;
+            this.panel1.Location = new System.Drawing.Point(214, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(371, 327);
+            this.panel1.TabIndex = 4;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.DoubleClick += new System.EventHandler(this.rbRectangle_CheckedChanged);
+            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pbPaint_MouseClick);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // tb1
+            // 
+            this.tb1.Location = new System.Drawing.Point(6, 86);
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(85, 20);
+            this.tb1.TabIndex = 5;
+            // 
+            // tb2
+            // 
+            this.tb2.Location = new System.Drawing.Point(6, 112);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(85, 20);
+            this.tb2.TabIndex = 6;
+            // 
+            // btcreate
+            // 
+            this.btcreate.Location = new System.Drawing.Point(120, 9);
+            this.btcreate.Name = "btcreate";
+            this.btcreate.Size = new System.Drawing.Size(76, 36);
+            this.btcreate.TabIndex = 7;
+            this.btcreate.Text = "Создать";
+            this.btcreate.UseVisualStyleBackColor = true;
+            this.btcreate.Click += new System.EventHandler(this.btcreate_Click);
+            // 
+            // btpar
+            // 
+            this.btpar.Location = new System.Drawing.Point(120, 60);
+            this.btpar.Name = "btpar";
+            this.btpar.Size = new System.Drawing.Size(76, 36);
+            this.btpar.TabIndex = 8;
+            this.btpar.Text = "Задать параметры";
+            this.btpar.UseVisualStyleBackColor = true;
+            this.btpar.Click += new System.EventHandler(this.btpar_Click);
+            // 
+            // rbwagon
+            // 
+            this.rbwagon.AutoSize = true;
+            this.rbwagon.Location = new System.Drawing.Point(6, 55);
+            this.rbwagon.Name = "rbwagon";
+            this.rbwagon.Size = new System.Drawing.Size(60, 17);
+            this.rbwagon.TabIndex = 9;
+            this.rbwagon.TabStop = true;
+            this.rbwagon.Text = "Wagon";
+            this.rbwagon.UseVisualStyleBackColor = true;
+            this.rbwagon.CheckedChanged += new System.EventHandler(this.rbwagon_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 389);
+            this.Controls.Add(this.rbwagon);
+            this.Controls.Add(this.btpar);
+            this.Controls.Add(this.btcreate);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.rbCircle);
+            this.Controls.Add(this.rbRectangle);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbRectangle;
+        private System.Windows.Forms.RadioButton rbCircle;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.TextBox tb2;
+        private System.Windows.Forms.Button btcreate;
+        private System.Windows.Forms.Button btpar;
+        private System.Windows.Forms.RadioButton rbwagon;
+    }
+}
+
---
+++ WindowsFormsApp2/MyWagon.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class MyWagon:MyRectangle
+    { 
+        public MyWagon() : base()
+        {
+
+        }
+        private int l = 20;
+        public override int L
+        {
+            get
+            {
+                return l;
+            }
+            set
+            {
+                l = value;
+            }
+        }
+        private void wheel (Graphics graphics, int L)
+        {
+            graphics.DrawEllipse(new Pen(Color.Black), x + L / 3-L/4, y + L / 2, L / 3, L / 3);
+            graphics.DrawEllipse(new Pen(Color.Black), x + L / 3 + L / 3, y + L / 2, L / 3, L / 3);
+        }
+        private int x, y;
+        public override int X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+        public override int Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+        public override void Draw(Graphics graphics)
+        {
+           graphics.DrawRectangle(new Pen(Color.Black),x,y,l,l/2);
+           wheel(graphics,l);
+        }
+    }
+}
